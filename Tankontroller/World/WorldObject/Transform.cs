04789c12830ee79d7b97658773ef3eb158094590
Piotr Moskala <70442053+SeminixDev@users.noreply.github.com>
2026-01-21 17:00:54 +0000

    Create Transform.cs

---
+++ Tankontroller/World/WorldObject/Transform.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Tankontroller.World.WorldObject
+{
+    /// <summary>
+    /// Stores position, rotation and scale of an object in the world
+    /// </summary>
+    internal class Transform
+    {
+        public Vector2 Position { get; set; }
+        public float Rotation { get; set; }
+        public Vector2 Scale { get; set; }
+
+        public Transform()
+        {
+            Position = Vector2.Zero;
+            Rotation = 0f;
+            Scale = Vector2.One;
+        }
+
+        public Transform(Vector2 pPosition)
+        {
+            Position = pPosition;
+            Rotation = 0f;
+            Scale = Vector2.One;
+        }
+
+        public Transform(Vector2 pPosition, float pRotation, Vector2 pScale)
+        {
+            Position = pPosition;
+            Rotation = pRotation;
+            Scale = pScale;
+        }
+    }
+}
