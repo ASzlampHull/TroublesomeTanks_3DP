08d6ca0cef23061d722a584cb8f3bf6bfce1d067
Piotr Moskala <70442053+SeminixDev@users.noreply.github.com>
2026-01-23 15:54:59 +0000

    Create required shape classes

---
+++ Tankontroller/World/Shapes/CircleShape.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Tankontroller.World.WorldObject;
+
+namespace Tankontroller.World.Shapes
+{
+    internal class CircleShape : Shape
+    {
+        public CircleShape(Transform pOwner, bool pEnabled = true) : base(pOwner, pEnabled)
+        {
+
+        }
+
+        public CircleShape(Transform pOwner, Vector2 pLocalOffset, bool pEnabled = true) : base(pOwner, pLocalOffset, pEnabled)
+        {
+
+        }
+
+        public override CollisionEvent Intersects(Shape other)
+        {
+
+            return new CollisionEvent(false);
+        }
+    }
+}
---
+++ Tankontroller/World/Shapes/RectangleAxisAlignedShape.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tankontroller.World.WorldObject;
+
+namespace Tankontroller.World.Shapes
+{
+    internal class RectangleAxisAlignedShape : Shape
+    {
+        public RectangleAxisAlignedShape(Transform pOwner, bool pEnabled = true) : base(pOwner, pEnabled)
+        {
+        }
+
+        public RectangleAxisAlignedShape(Transform pOwner, Vector2 pLocalOffset, bool pEnabled = true) : base(pOwner, pLocalOffset, pEnabled)
+        {
+        }
+
+        public override CollisionEvent Intersects(Shape other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
