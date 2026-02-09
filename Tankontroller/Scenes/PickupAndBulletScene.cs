2fb90262a9669f8bdafbd7c2f6e4f2c697b16609
ASzlampHull <A.SZLAMP-2023@hull.ac.uk>
2026-02-09 16:28:46 +0000

    Remove any reference to info texture.

--- Tankontroller/Scenes/PickupAndBulletScene.cs
+++ Tankontroller/Scenes/PickupAndBulletScene.cs
@@ -12,11 +12,9 @@ using static System.Formats.Asn1.AsnWriter;
 
 namespace Tankontroller.Scenes
 {
-    //This is not created correctly as the scene is just one static image that cannot be changed
     public class PickupAndBulletScene : IScene
     {
         private static readonly Texture2D mBackgroundTexture = Tankontroller.Instance().CM().Load<Texture2D>("background_01");
-        private static readonly Texture2D infoTexture = Tankontroller.Instance().CM().Load<Texture2D>("pickupinfoscreen");
         private Rectangle mBackgroundRectangle;
         private Rectangle mPickupinfoRectangle;
         private Tankontroller mGameInstance;
@@ -41,7 +39,6 @@ namespace Tankontroller.Scenes
             mContinueButtonRectangle = new Rectangle(10, screenHeight / 2, mContinueButtonTexture.Width / 2, mContinueButtonTexture.Height / 2);
             mContinueTextTexture = game.CM().Load<Texture2D>("back");
             mContinueTextRectangle = new Rectangle(20 + mContinueButtonTexture.Width / 2, screenHeight / 2 + mContinueButtonTexture.Height / 4, mContinueTextTexture.Width, mContinueTextTexture.Height);
-            mPickupinfoRectangle = new Rectangle(0, 0, infoTexture.Width, infoTexture.Height);
 
             GenerateButtons();
         }
@@ -51,7 +48,6 @@ namespace Tankontroller.Scenes
             mGameInstance.GDM().GraphicsDevice.Clear(Color.Black);
             spriteBatch.Begin();
             spriteBatch.Draw(mBackgroundTexture, mBackgroundRectangle, Color.White);
-            //spriteBatch.Draw(infoTexture, mPickupinfoRectangle, Color.White);
             mButtonList.Draw(spriteBatch);
             spriteBatch.Draw(mContinueButtonTexture, mContinueButtonRectangle, Color.White);
             spriteBatch.Draw(mContinueTextTexture, mContinueTextRectangle, Color.White);
